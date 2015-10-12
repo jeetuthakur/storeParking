dc63e9e586ab108056335d5a1bf7e3bc87e335c3
jeetuthakur <jitu.thakur1805@gmail.com>
2015-10-12 20:44:42 +0530

    asdf

--- config/database.js
+++ config/database.js
@@ -1,6 +1,16 @@
+// default to a 'localhost' configuration:
+var connection_string = 'localhost:27017/YOUR_APP_NAME';
+
+// if OPENSHIFT env variables are present, use the available connection info:
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + "parkingdb";
+}
+
 module.exports = {
 
-	// the database url to connect
-    url : 'mongodb://admin:4wMs94AECgVn@127.13.122.130:$OPENSHIFT_MONGODB_DB_PORT/parkingdb'
-	//		url : 'mongodb://localhost/parkingdb'
+    url : 'mongodb:' + connection_string
+//		url : 'mongodb://localhost/parkingdb'
 }
