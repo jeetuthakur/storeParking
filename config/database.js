6ad3235312f6182c50848f66cb6150db05572b60
jeetuthakur <jitu.thakur1805@gmail.com>
2015-10-12 08:20:28 +0530

    asdf

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
 	// the database url to connect
-    url : 'mongodb://admin:VMlvSNeCuarF@127.13.122.130:$OPENSHIFT_MONGODB_DB_PORT/storeParking'
+    url : 'mongodb://admin:VMlvSNeCuarF@127.13.122.130:$OPENSHIFT_MONGODB_DB_PORT/parkingdb'
 	//		url : 'mongodb://localhost/parkingdb'
 }
